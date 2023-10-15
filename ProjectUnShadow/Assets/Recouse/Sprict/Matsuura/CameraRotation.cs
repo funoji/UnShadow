dc9f39971cec6c14a1e1e57399dddf476582d20a
matsuurataketo <90751819+matsuurataketo@users.noreply.github.com>
2023-10-15 17:15:00 +0900

    カメラの回転を変えました

--- ProjectUnShadow/Assets/Recouse/Sprict/Matsuura/CameraRotation.cs
+++ ProjectUnShadow/Assets/Recouse/Sprict/Matsuura/CameraRotation.cs
@@ -4,12 +4,15 @@ using Cinemachine;
 public class CameraRotation : MonoBehaviour
 {
     public CinemachineVirtualCamera virtualCamera; // Cinemachine Virtual Cameraを参照するための変数
+    bool K;//回転させるフラグ
+    float count = 0;
 
     private void Start()
     {
         // virtualCameraがnullでないことを確認
         if (virtualCamera == null)
         {
+            K = false;
             Debug.LogError("Cinemachine Virtual Cameraがアタッチされていません。");
             return;
         }
@@ -18,13 +21,23 @@ public class CameraRotation : MonoBehaviour
     private void Update()
     {
         // Fキーが押されたときにHorizontal AxisのValueを変更する
-        if (Input.GetKeyDown(KeyCode.F))
+        if (Input.GetKeyDown(KeyCode.F)&&count==0)
+            K = true;
+
+        if (K == true)
         {
             // Cinemachine Virtual CameraのPOVを取得
             var pov = virtualCamera.GetCinemachineComponent<CinemachinePOV>();
 
             // Horizontal AxisのValueを変更（例: 90度）
-            pov.m_HorizontalAxis.Value += 90f;
+            pov.m_HorizontalAxis.Value++;
+            count++;
+            if (count == 90)
+            {
+                K = false;
+                count = 0;
+            }
         }
+        
     }
 }
